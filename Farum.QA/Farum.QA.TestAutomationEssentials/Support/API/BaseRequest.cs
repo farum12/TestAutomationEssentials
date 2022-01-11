b34172d3101926eb2c2a2e180b877b4724f46c55
Piotr Niedziałek <piotr.niedzialek@outlook.com>
2022-01-11 21:16:56 +0100

    Added BaseRequest; downgraded RestSharp

---
+++ Farum.QA/Farum.QA.TestAutomationEssentials/Support/API/BaseRequest.cs
@@ -0,0 +1,35 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farum.QA.TestAutomationEssentials.Support.API
+{
+    /// <summary>
+    /// Class intended for creation of Requests. 
+    /// </summary>
+    public class BaseRequest
+    {
+        protected RestClient client;
+        protected RestRequest request;
+
+        protected BaseRequest(string url, Method httpMethod)
+        {
+            client = new RestClient(url);
+            client.Timeout = -1;
+            request = new RestRequest(httpMethod);
+            request.AddHeader("authorization", "SOMEAUTHORIZATION");
+            request.AddHeader("Content-Type", "application/json");
+        }
+
+        protected void SetPayload(string json)
+        {
+            request.AddParameter("application/json", json, ParameterType.RequestBody);
+        }
+
+        protected IRestResponse SendRequest()
+        {
+            return client.Execute(request);
+        }
+    }
+}
